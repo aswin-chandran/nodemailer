3c18abe5f141ffe2d805ade9b794a924d3ebf60e
aswin chandran <aswin1998ply@gmail.com>
2023-10-31 20:02:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+let mailTransporter = nodemailer.createTransport({
+
+    service:"gmail",
+    auth:{
+        user:"aswin1998ply@gmail.com",
+        pass : "tnef anfx hnwk huwv"
+    }
+})
+
+let details = {
+    from:"aswin1998ply@gmail.com",
+    to :"aj98ply@gmail.com",
+    subject :" testing nodemailer",
+    text :"testing sender",
+
+
+}
+
+mailTransporter.sendMail(details,(err)=>{
+    if(err){
+        console.log("it has an error",err)
+    }
+    else{
+        console.log('Email has been send')
+    }
+})
